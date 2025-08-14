7d22dee39c49b63c36805e33beef42b77f835dca
knovaera-debug <k.novaera@gmail.com>
2025-08-14 21:01:34 +0900

    Delete get_deps.ps1

--- get_deps.ps1
+++
@@ -1,7 +0,0 @@
-# 依存ファイル（Leaflet/JSZip/toGeoJSON）
-$ErrorActionPreference = "Stop"
-function Get-File($Url, $OutPath) { Invoke-WebRequest -Uri $Url -OutFile $OutPath -UseBasicParsing }
-Get-File "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" "leaflet.js"
-Get-File "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" "leaflet.css"
-Get-File "https://unpkg.com/@tmcw/togeojson@5.7.0/dist/togeojson.umd.js" "togeojson.umd.js"
-Get-File "https://cdn.jsdelivr.net/npm/jszip@3.10.1/dist/jszip.min.js" "jszip.min.js"
